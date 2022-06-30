f651611d64d0f0c65f1e9b42cf343e55951aa50f
Mortemir <gesichtsloser@gmail.com>
2022-06-30 16:22:26 +0300

    Initial Commit

---
+++ JourneyIntoUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JourneyIntoUI
+//
+//  Created by Ян on 6/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
